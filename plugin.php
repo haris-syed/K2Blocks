29276466dea8541ef7ffd4023bbd5327e490c127
Harry <syedharis632@gmail.com>
2020-05-19 20:11:28 +0500

    name change

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: K2 Blocks for Gutenberg
+ * Plugin Name: K2 Blocks
  * Plugin URI: https://k2blocks.com/
  * Description: Blocks for the gutenberg editor
  * Author: PookiDevs
